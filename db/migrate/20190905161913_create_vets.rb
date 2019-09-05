a7600077fb122b5b674a69c947d1a1db1fada70a
Eric Kim <hyeokjungkim@gmail.com>
2019-09-05 13:09:04 -0400

    removed specifics

--- db/migrate/20190905161913_create_vets.rb
+++
@@ -1,9 +0,0 @@
-class CreateVets < ActiveRecord::Migration[5.2]
-  def change
-    create_table :vets do |t|
-      t.string :name
-      t.string :location
-      t.integer :years
-    end
-  end
-end
